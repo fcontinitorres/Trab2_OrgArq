063f0cf86f905a9115190f43249895c43a51f496
Julia Diniz <judinizf@gmail.com>
2017-07-02 18:11:47 -0300

    estatistica.c

---
+++ src/estatistica.c
@@ -0,0 +1,12 @@
+/*
+GRUPO G5
+Felipe Scrochio Custodio, 9442688
+Felipe Contini Torres, 9253670
+Júlia Diniz Ferreira, 9364865
+Bruno Henrique Rasteiro, 9292910
+*/
+
+
+int quant_entradas(){
+	
+}
